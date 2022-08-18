0a05be50cd1ea0fc5382b3a9ab4a212292e420a3
TheChiran <tonmoychiran449@gmail.com>
2022-08-18 10:02:05 +0600

    [Task-> 1. Added required color variables]

--- src/components/main/App.tsx
+++ src/components/main/App.tsx
@@ -4,11 +4,14 @@ import "./../../assets/styles/style.scss";
 
 const App: React.FC = () => {
     const theme = React.useContext(ThemeContext);
+    const changeTheme = ()=>{
+      document.documentElement.style.setProperty('--bg-color','#2f3237');
+    }
 
     if(theme.layout === 'default'){
       return (
         <div className='container'>
-        
+        <button onClick={changeTheme}>Change theme</button>
         <footer>
           <p>Created by <a href="https://chirantonmoy.vercel.app/">chiranSWE</a> - devChallenges.io</p>
         </footer>
